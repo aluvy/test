3ae9d7344797737a4ab8fe7be561940039a8ee65
songmiok <aluvy_@naver.com>
2024-05-10 14:22:07 +0900

    Update App.java

--- algorithm/src/App.java
+++ algorithm/src/App.java
@@ -5,12 +5,14 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         try (Scanner sc = new Scanner(System.in)) {
-            // 랜덤 숫자 생성
+            
             ArrayList<Integer> dataSet = new ArrayList<Integer>();
             
-            // 숫자 입력받기
+            // 요구사항1: 사용자로터 10개의 숫자를 입력 받는다.
             for ( int i=0; i<10; i++) {
                 System.out.println(i+1 + "번째 숫자를 입력해 주세요");
+
+                // 요구사항2: 입력 받은 숫자는 리스트나 배열에 저장된다.
                 dataSet.add(sc.nextInt());
             }
             System.out.println("--- 정렬 전: " + dataSet);
